d2687d798843583003f4d627e3782dcc424a296d
Mickey <msmith@digitalprimates.net>
2020-10-08 19:57:39 -0500

    end of article 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  Chart,
+  ChartTitle,
+  ChartSeries,
+  ChartLegend,
+  ChartSeriesItem,
+  ChartCategoryAxis,
+  ChartCategoryAxisItem,
+} from "@progress/kendo-react-charts";
+import "hammerjs";
 import './App.css';
 
 function App() {
-  return (<></>);
+  return (
+    <>
+      <Chart>
+        <ChartTitle text={"Test Chart Title"} />
+        <ChartCategoryAxis>
+          <ChartCategoryAxisItem
+            categories={["category1", "category2", "category3"]}
+          />
+        </ChartCategoryAxis>
+        <ChartLegend position="bottom" orientation="horizontal" />
+        <ChartSeries>
+          <ChartSeriesItem
+            type="column"
+            data={[1, 1, 1]}
+          />
+          <ChartSeriesItem
+            type="column"
+            data={[2, 2, 2]}
+          />
+          <ChartSeriesItem
+            type="column"
+            data={[3, 3, 3]}
+          />
+        </ChartSeries>
+      </Chart>
+    </>
+  );
 }
 
 export default App;
